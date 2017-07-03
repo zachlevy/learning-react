98cf88e172a7091669eb9efaab9fbfaa15a2c5f1
Zachary Levy <zacharyalevy@gmail.com>
2017-07-03 16:13:34 -0400

    thunk and counter example

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import Home from '../Home'
 import Courses from '../Courses'
+import Counter from '../Counter'
 
 class App extends Component {
   render() {
@@ -12,11 +13,13 @@ class App extends Component {
         <header>
           <Link to="/">Home</Link>
           <Link to="/courses">Courses</Link>
+          <Link to="/counter">Counter</Link>
         </header>
 
         <main>
           <Route exact path="/" component={Home} />
           <Route exact path="/courses" component={Courses} />
+          <Route exact path="/counter" component={Counter} />
         </main>
       </div>
     )

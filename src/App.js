ca188863e7106d76fea0abd1a763c7310552c0a3
Zachary Levy <zacharyalevy@gmail.com>
2017-07-03 00:16:59 -0400

    componentize

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import Courses from './Courses'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      courses: []
-    }
-  }
-  componentDidMount() {
-    console.log("componentDidLoad")
-    fetch("/courses").then((res) => {
-      return res.json()
-    }).then((response) => {
-      console.log(response)
-      this.setState({courses: response})
-    })
-  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {
-          this.state.courses.map((course, index) => {
-            return <p key={index}>{course.title}</p>
-          })
-        }
+      <div className="container">
+        <Courses />
       </div>
     );
   }
---
+++ src/Courses/Course.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+class Course extends Component {
+  render() {
+    return (
+      <div className="col-4">
+        <p>{this.props.course.title}</p>
+      </div>
+    );
+  }
+}
+
+Course.propTypes = {
+  course: PropTypes.object
+}
+
+export default Course;

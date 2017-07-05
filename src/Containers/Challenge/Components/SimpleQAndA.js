6fa1604b73d4928945f2f54f725d8d67c9fa36f1
Zachary Levy <zacharyalevy@gmail.com>
2017-07-05 18:03:00 -0400

    next button on content page

--- src/Containers/Challenge/Components/SimpleQAndA.js
+++ src/Containers/Challenge/Components/SimpleQAndA.js
@@ -18,10 +18,24 @@ class SimpleQAndA extends Component {
   render() {
     return (
       <div className="container">
-        <p>Question: {this.props.question}</p>
-        <p>Answer: {this.props.answer}</p>
-        <input onKeyUp={this.handleKeyUp.bind(this)} />
-        <button onClick={this.assert.bind(this)}>Check</button>
+        <div className="row">
+          <div className="col-12">
+            <p>Question: {this.props.question}</p>
+            <p>Answer: {this.props.answer}</p>
+            <input onKeyUp={this.handleKeyUp.bind(this)} />
+            <button onClick={this.assert.bind(this)}>Check</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-10">
+            <div className="float-md-right">
+              <br />
+              <button className="btn btn-secondary" onClick={this.props.handleSkipClick.bind(this)}>Skip</button>
+              &nbsp;
+              <button className="btn btn-primary" onClick={this.props.handleNextClick.bind(this)}>Next</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }

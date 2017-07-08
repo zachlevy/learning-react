fb41f658508bc4021fe04fc9f6b3ad716dcce046
Zachary Levy <zacharyalevy@gmail.com>
2017-07-08 19:35:48 -0400

    basic landing and ui

--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -2,15 +2,30 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import Courses from '../Courses'
 
 const Home = props => (
-  <div className="container">
-    <div className="row">
-      <div className="col-12">
-        <h1>Home</h1>
-        <p>Welcome home!</p>
+  <div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 text-center bg-gradient">
+          <br />
+          <br />
+          <br />
+          <br />
+          <h2>You can learn anything.</h2>
+          <h2>For free. For everyone. Forever.</h2>
+          <br />
+          <button className="btn btn-primary">Let's do it</button>
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
       </div>
     </div>
+    <br />
+    <Courses />
   </div>
 )
 

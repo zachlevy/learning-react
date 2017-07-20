065b040b10ab22313d6445ff0688c592f9b6ff03
Zachary Levy <zacharyalevy@gmail.com>
2017-07-20 17:28:53 -0400

    dictionary component for terms

---
+++ src/Containers/Shared/Dictionary.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { Button, Popover, PopoverTitle, PopoverContent } from 'reactstrap'
+
+class Dictionary extends Component {
+  constructor() {
+    super()
+    this.state = {
+      popoverOpen: false
+    }
+  }
+  toggle() {
+    console.log("this.state", this.state)
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+  render() {
+    console.log("dictionary key", this.props.key)
+    let link
+    if (this.props.link) {
+      link = (
+        <a href={this.props.link} target="_blank"><br />Read More</a>
+      )
+    }
+    return (
+      <div className="dictionary-wrapper">
+        <button className="btn-link-inline" id={this.props.index} onClick={this.toggle.bind(this)}>
+          {this.props.term}
+        </button>
+        <Popover placement="top" isOpen={this.state.popoverOpen} target={this.props.index} toggle={this.toggle.bind(this)}>
+          <PopoverTitle>{this.props.term}</PopoverTitle>
+          <PopoverContent>{this.props.definition}{link}</PopoverContent>
+        </Popover>
+      </div>
+    )
+  }
+}
+
+Dictionary.propTypes = {
+  term: PropTypes.string,
+  definition: PropTypes.string,
+  link: PropTypes.string,
+  index: PropTypes.string
+}
+
+export default Dictionary

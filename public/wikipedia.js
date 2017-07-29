8ae92067f53d1ddef89548591275604c4e1541a9
Zachary Levy <zacharyalevy@gmail.com>
2017-07-29 15:04:12 -0400

    adding logo to specific paragraphs

--- public/wikipedia.js
+++ public/wikipedia.js
@@ -1,2 +1,40 @@
 // this file modifies wikipedia when adding it to the head tag
-document.getElementById("section_0").innerHTML = "oh heyy"
+// jquery not available in this file
+
+var sectionTitleText = "Early life"
+var sectionTitleId = sectionTitleText.replace(" ", "_")
+
+// console.log("sectionTitleText", sectionTitleText)
+// console.log("sectionTitleId", sectionTitleId)
+
+// get the sections by id
+var sectionTitle = document.getElementById(sectionTitleId).parentElement
+var sectionBody = sectionTitle.nextSibling
+
+// console.log("sectionTitle", sectionTitle)
+// console.log("sectionBody", sectionBody)
+
+// open the blocks
+sectionTitle.className += " open-block"
+sectionBody.className += " open-block"
+
+// scroll to the section
+document.getElementById(sectionTitleId).scrollIntoView()
+
+// console.log("sectionBody.children", sectionBody.children)
+
+var sectionParagraphs = []
+for (var i = 0; i < sectionBody.children.length; i++) {
+  // make sure it's a paragraph
+  if (sectionBody.children[i].tagName === "P") {
+    sectionParagraphs.push(sectionBody.children[i])
+  }
+}
+
+// console.log("sectionParagraphs", sectionParagraphs)
+
+sectionParagraphs.forEach(function(p, index) {
+  if (index === 0) {
+    p.className += " vora-logo"
+  }
+})

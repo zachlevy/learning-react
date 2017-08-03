90cb62d00b5c9250eb6f97e621dde6ef65906fa3
Zachary Levy <zacharyalevy@gmail.com>
2017-08-03 16:16:57 -0400

    bugfix wikipedia icons, youtube controls

--- src/Containers/Challenge/Components/YoutubeVideo.js
+++ src/Containers/Challenge/Components/YoutubeVideo.js
@@ -128,7 +128,7 @@ class YoutubeVideo extends Component {
                   autoplay: 0,
                   modestbranding: 1,
                   showinfo: 0,
-                  // controls: 0,
+                  controls: 0,
                   iv_load_policy: 3,
                   start: content.start_seconds,
                   end: content.end_seconds

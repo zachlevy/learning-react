71742bb5d16dcf7a79af65a5f8ec14db9e536466
Zachary Levy <zacharyalevy@gmail.com>
2017-08-05 14:31:19 -0400

    disable after youtube video suggestions

--- src/Containers/Challenge/Components/YoutubeVideo.js
+++ src/Containers/Challenge/Components/YoutubeVideo.js
@@ -129,6 +129,7 @@ class YoutubeVideo extends Component {
                   modestbranding: 1,
                   showinfo: 0,
                   controls: 0,
+                  rel: 0,
                   iv_load_policy: 3,
                   start: content.start_seconds,
                   end: content.end_seconds

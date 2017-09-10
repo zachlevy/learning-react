bb1f35cc4306f42df3d65bcb4811190275c5831a
Zach Levy <zacharyalevy@gmail.com>
2017-09-10 14:54:03 -0400

    user create form submission

--- src/Containers/Users/New.js
+++ src/Containers/Users/New.js
@@ -7,12 +7,35 @@ class New extends Component {
 
   handleSubmit(userValues) {
     console.log("handleSubmit", userValues)
+    fetch(`${process.env.REACT_APP_API_URL}/users`, {
+      method: 'post',
+      body: JSON.stringify({
+        user: {
+          email: userValues.email,
+          password: userValues.password,
+          password_confirmation: userValues.passwordConfirmation
+        }
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((res) => {
+      return res.json()
+    }).then((response) => {
+      console.log(response)
+    })
   }
 
   render() {
     return (
-      <div>
-        <UserForm onSubmit={this.handleSubmit}/>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-4 offset-sm-4">
+            <br />
+            <h4>Register</h4>
+            <UserForm onSubmit={this.handleSubmit}/>
+          </div>
+        </div>
       </div>
     )
   }
--- src/Containers/Users/UserForm.js
+++ src/Containers/Users/UserForm.js
@@ -8,8 +8,19 @@ class UserForm extends Component {
     return (
       <div>
         <form onSubmit={ this.props.handleSubmit }>
-          <Field name="email" component="input" type="email" />
-          <button type="submit">Submit</button>
+          <div className="form-group">
+            <label>Email</label>
+            <Field className="form-control" name="email" component="input" type="email" />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <Field className="form-control" name="password" component="input" type="password" />
+          </div>
+          <div className="form-group">
+            <label>Password Confirmation</label>
+            <Field className="form-control" name="passwordConfirmation" component="input" type="password" />
+          </div>
+          <button className="btn btn-primary btn-block" type="submit">Submit</button>
         </form>
       </div>
     )

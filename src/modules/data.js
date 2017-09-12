6683f417d625f8088a13dde67b787d9fede2d8c2
Zachary Levy <zacharyalevy@gmail.com>
2017-09-12 12:15:22 -0400

    refactor jwt requests

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,19 +1,26 @@
+import store from '../store'
+
 // converts rails api errors object to array strings
 // options is the same as a fetch
 // headers are deep merged
-export const apiRequest = (endpoint, options, callback, jwt) => {
+export const apiRequest = (endpoint, options, callback) => {
   const defaultOptions = {
     method: "get",
     headers: {
       "Content-Type": "application/json",
     }
   }
+
+  // get auth token
+  const jwt = store.getState().user.jwt
   if (jwt) {
     defaultOptions.headers["Authorization"] = `Bearer ${jwt}`
   }
+  // pseudo deep merge
   const mergedHeaders = Object.assign({}, defaultOptions.headers, options.headers)
   const mergedOptions = Object.assign({}, defaultOptions, options, {headers: mergedHeaders})
 
+  // request
   fetch(`${process.env.REACT_APP_API_URL}${endpoint}`, mergedOptions).then((res) => {
     if(res.headers.get("content-type").includes("application/json")) {
       res.json().then((response) => {
@@ -31,12 +38,7 @@ export const apiRequest = (endpoint, options, callback, jwt) => {
 }
 
 export const getCurrentUser = (jwt, callback) => {
-  apiRequest("/users/me", {
-    method: 'get',
-    headers: {
-      "Authorization": `Bearer ${jwt}`
-    }
-  }, (response, status) => {
+  apiRequest("/users/me", {}, (response, status) => {
     callback(response)
   })
 }

4dd3711ce6b2b82220a59027bbe854b79978d28e
Zachary Levy <zacharyalevy@gmail.com>
2017-09-13 11:27:31 -0400

    build json form on challenge type change

--- src/Containers/Admin/Challenges/ChallengeForm.js
+++ src/Containers/Admin/Challenges/ChallengeForm.js
@@ -2,19 +2,52 @@ import React, { Component } from 'react'
 import { Field, reduxForm, FieldArray } from 'redux-form'
 import { parseApiErrors } from '../../../modules/strings'
 import { defaultChallenge } from '../../../modules/defaults'
+import { apiRequest } from '../../../modules/data'
 import { buildFormFields, buildFormErrors } from '../../../modules/forms'
 
 class ChallengeForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      challengeTypes: [],
+      selectedChallengeType: null
+    }
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount")
+    apiRequest(`/challenge_types`, {
+    }, (response, status) => {
+      console.log("challenge_types", response)
+      this.setState({challengeTypes: response})
+    })
+  }
+
+  handleChallengeTypeChange(e) {
+    console.log("handleChallengeTypeChange", e.target.value)
+    const selectedChallengeType = this.state.challengeTypes.find((challengeType) => {return challengeType.id == e.target.value})
+    this.setState({selectedChallengeType: selectedChallengeType})
+  }
+
   render() {
     const errors = buildFormErrors(this.props.errors)
     const blacklistKeys = []
+    let mergedChallengeBody
+    if (this.state.selectedChallengeType) {
+      mergedChallengeBody = Object.assign({}, defaultChallenge, {body: this.state.selectedChallengeType.template_data})
+    } else {
+      mergedChallengeBody = defaultChallenge
+    }
     return (
       <div>
         {errors}
+        <pre>{JSON.stringify(mergedChallengeBody)}</pre>
         <form onSubmit={ this.props.handleSubmit }>
           <div className="form-group">
+            <label>Challenge Type Id</label>
+            <Field onChange={this.handleChallengeTypeChange.bind(this)} className="form-control" name="challenge_type_id" component="input" type="text" />
             {
-              buildFormFields(defaultChallenge, blacklistKeys)
+              buildFormFields(mergedChallengeBody, blacklistKeys)
             }
           </div>
           <button className="btn btn-primary btn-block" type="submit">Submit</button>

9d45b59208e329a9256fb3e8ad105ec3bf346348
Zachary Levy <zacharyalevy@gmail.com>
2017-10-02 22:56:59 -0400

    api plus redux

--- src/modules/redux/profile.js
+++ src/modules/redux/profile.js
@@ -1,6 +1,8 @@
 export const SET_PROFILE = 'profile/SET_PROFILE'
 export const CLEAR_PROFILE = 'profile/CLEAR_PROFILE'
 
+import { apiRequest } from '../data'
+
 const initialState = {}
 
 export default (state = initialState, action) => {
@@ -14,6 +16,35 @@ export default (state = initialState, action) => {
 
     default:
       return state
+  }
+}
+
+export const getAndSetProfileFromApi = () => {
+  return dispatch => {
+    apiRequest(`/profiles/me`, {}, (response, status) => {
+      if (status === 200) {
+        dispatch({
+          type: SET_PROFILE,
+          response
+        })
+      }
+    })
+  }
+}
+
+export const updateAndSetProfileFromApi = (profile) => {
+  return dispatch => {
+    apiRequest(`/profiles/me`, {
+      method: "put",
+      body: JSON.stringify({profile: profile})
+    }, (response, status) => {
+      if (status === 200) {
+        dispatch({
+          type: SET_PROFILE,
+          response
+        })
+      }
+    })
   }
 }
 

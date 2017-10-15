026caf4d7b6dc62584dcf091f8154daf7e68c95a
Zachary Levy <zacharyalevy@gmail.com>
2017-10-15 17:41:02 -0400

    add continue from latest point, restart

--- src/Containers/Courses/CourseThumb.js
+++ src/Containers/Courses/CourseThumb.js
@@ -5,20 +5,55 @@ import { Link } from 'react-router-dom'
 import { secondsToMinutes } from '../../modules/time'
 import { gradientBackground } from '../../modules/styles'
 import FontAwesome from 'react-fontawesome'
+import { apiRequest } from '../../modules/data'
 
 // used to render the course thumbnails in course lists
 class CourseThumb extends Component {
+  constructor() {
+    super()
+    this.state = {
+      firstChallengeId: null,
+      completed: false
+    }
+  }
+
+  componentDidMount() {
+    // get the next challenge without a challenge response
+    if (this.props.course.attempted === true) {
+      apiRequest(`/challenge_responses?course_id=${this.props.course.id}`, {}, (response, status) => {
+        if (status === 200) {
+          if (response.length > 0) {
+            const lastChallengeResponse = response[response.length - 1]
+            const lastChallengeFlowIndex = this.props.course.flow.findIndex((flowChallenge) => {
+              return flowChallenge.id === lastChallengeResponse.challenge_id
+            })
+            const nextChallenge = this.props.course.flow[lastChallengeFlowIndex + 1]
+            // check if it's the last challenge in the flow
+            if (!nextChallenge) {
+              this.setState({completed: true})
+            } else {
+              this.setState({firstChallengeId: nextChallenge.id || (this.props.course.flow[0] && this.props.course.flow[0].id)})
+            }
+          }
+        }
+      })
+    }
+  }
   render() {
     const course = this.props.course
     let link
     if (course.flow[0]) {
       let text
       if (course.attempted) {
-        text = "Continue"
+        if (this.state.completed) {
+          text = "Restart"
+        } else {
+          text = "Continue"
+        }
       } else {
         text = "Start"
       }
-      link = <Link className="btn btn-outline-secondary btn-lg" to={`/courses/${course.id}/challenges/${course.flow[0].id}`}>{text}</Link>
+      link = <Link className="btn btn-outline-secondary btn-lg" to={`/courses/${course.id}/challenges/${this.state.firstChallengeId || course.flow[0].id}`}>{text}</Link>
     }
     let review
     if (course.attempted) {
